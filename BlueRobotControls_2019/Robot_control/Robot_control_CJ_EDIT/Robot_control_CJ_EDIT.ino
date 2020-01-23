d0d49f0d21ff585841797a63e31e7a7d5a5ae2bf
csando6 <csando6@uic.edu>
2020-01-22 22:39:16 -0600

    value changes based on oldVal-currVal, to prevent drastic changes

--- BlueRobotControls_2019/Robot_control/Robot_control_CJ_EDIT/Robot_control_CJ_EDIT.ino
+++ BlueRobotControls_2019/Robot_control/Robot_control_CJ_EDIT/Robot_control_CJ_EDIT.ino
@@ -1,4 +1,4 @@
-//Made by Brendon Brown
+ //Made by Brendon Brown
 //Last updated March 11th, 2019
 //This code takes the serial data it has received from the xbee and performs actions accordingly
 //Pins 5 and 6 have PWM frequency of ~980 Hz and the rest ~490 Hz
@@ -11,8 +11,10 @@
 
 #include <Servo.h>
 //note when using the servo library the frequency is not important because the way servo signals work, be sure to understand why this is...
-int COUNTER = 3;
-int timer[2] = {0,0};
+int COUNTER = 3;  //remove
+int timer[2] = {0,0};//remove
+int currVal[2] = {95,95}; //will hold the live update
+int lastVal[2] = {95,95}; //will update to the currVal, will prevent sudden jerk of motors
 
 Servo leftmotor;
 Servo rightmotor;
@@ -46,68 +48,54 @@ void setup() {
 
 void loop() {
   //Testing phase of the code
-  Serial.print("Values-C-L-R-VL-VR-T0-T1:");
-//  Serial.print(controlinput);
-//  Serial.print(" ");
-//  Serial.print(lastleft);
-//  Serial.print(" ");
-//  Serial.print(lastright);
-//  Serial.print(" ");
-  Serial.print(variableleft);
+  Serial.print("Values-C-CL-CR-LL-LR:");
+  Serial.print(controlinput);
   Serial.print(" ");
-  Serial.print(variableright);
+//  Serial.print(currVal[0]);
+//  Serial.print(" ");
+//  Serial.print(currVal[1]);
+//  Serial.print(" ");
+  Serial.print(lastVal[0]);
   Serial.print(" ");
-  Serial.print(timer[0]);
-  Serial.print(" ");
-  Serial.print(timer[1]);
+  Serial.print(lastVal[1]);
   Serial.print(" ");
   Serial.print("\n");
+  
   //only update when there is serialdata incoming
   if(Serial.available()>0){ 
     controlinput = Serial.read();
     
     //This is for the motor control
-    if(controlinput == '0'){
-      lastleft = '0';
-      countl = 0;
+    if(controlinput == '0'){ //val = 95 //stationary
+      currVal[0] = 95;
     }
-    else if(controlinput == '1'){
-      lastright = '1';
-      countr = 0;
+    else if(controlinput == '2'){ //val = 120
+      currVal[0]=120;
     }
-    else if(controlinput == '2'){
-      lastleft = '2';
-      countl = 0;
+    else if(controlinput == '4'){ //val = 60
+      currVal[0]=60;
     }
-    else if(controlinput == '3'){
-      lastright= '3';
-      countr = 0;
+    else if(controlinput == '6'){ //val= 150
+      currVal[0]=150;
     }
-    else if(controlinput == '4'){
-      lastleft = '4';
-      countl = 0;
+    else if(controlinput == '8'){ //val = 80
+      currVal[0]=30;
     }
-    else if(controlinput == '5'){
-      lastright= '5';
-      countr = 0;
+    else if(controlinput == '1'){ //val = 95
+      currVal[1]=95;
     }
-    else if(controlinput == '6'){
-      lastleft = '6';
-      countl = 0;
+    else if(controlinput == '3'){ //val = 120
+      currVal[1]=120;
     }
-    else if(controlinput == '7'){
-      lastright= '7';
-      countr = 0;
+    else if(controlinput == '5'){ //val = 60
+      currVal[1]=60;
     }
-    else if(controlinput == '8'){
-      lastleft = '8';
-      countl = 0;
+    else if(controlinput == '7'){ //val = 150
+      currVal[1]=150;
     }
-    else if(controlinput == '9'){
-      lastright= '9';
-      countr = 0;
+    else if(controlinput == '9'){ //val = 30
+      currVal[1]=30;
     }
-    //This is for the arm base
     else if(controlinput == 'A'){
       if(armbasepos < 180){
         armbasepos = armbasepos + 10;
@@ -159,130 +147,32 @@ void loop() {
     digitalWrite(armupper0, LOW);
     digitalWrite(armupper1, LOW);
   }
-  //The variables and the limits on them need to be adjusted for each motor controller as they might be too fast or the signal you are sending is just out of range of acceptable input
-  //for the motor controller we used the center servo signal wasn't 90 and I had to guess until I found it which ended up being around 95
-  //some motor controllers are programmable so this process of guess and check isn't necessary
-  if(lastleft == '0'){
-    variableleft = 95;
+  
+  //update the currVal to lastVal-can implement 
+  if(currVal[0] > lastVal[0] ){
+    lastVal[0]++;
+    leftmotor.write(lastVal[0] );
   }
-  else if(lastleft == '2' && timer[0]==0){
-      timer[0]++;
-      if(variableleft < 120){
-        variableleft++;
-      }
-      else if(variableleft > 120){
-        variableleft--;
-      }
-  }
-  else if(lastleft == '4' && timer[0]==0){
-      timer[0]++;
-      if(variableleft > 60){
-        variableleft--;
-      }
-      else if(variableleft < 60){
-        variableleft++;
-      }
-  }
-  else if(lastleft == '6' && timer[0]==0){
-      timer[0]++;
-      if(variableleft < 150){
-        variableleft++;
-      }
-      else if(variableleft > 150){
-        variableleft--;
-      }
-  }
-  else if(lastleft == '8' && timer[0]==0){
-      timer[0]++;
-      if(variableleft > 30){
-        variableleft--;
-      }
-      else if(variableleft < 30){
-        variableleft++;
-      }
+  else if(currVal[0] < lastVal[0] ){
+    lastVal[0]--;
+    leftmotor.write(lastVal[0] );
   }
   else{
-    if(timer[0]>=COUNTER){
-      timer[0] = 0;
-    }
-    else{
-      timer[0]++;
-    }
+    leftmotor.write(lastVal[0] );
   }
 
-  if(lastright == '1'){
-    variableright = 95;
+  if(currVal[1] > lastVal[1] ){
+    lastVal[1]++;
+    rightmotor.write(lastVal[1] );
   }
-  else if(lastright == '3' && timer[1]==0){
-      timer[1]++;
-      if(variableright < 120){
-        variableright++;
-      }
-      else if(variableright > 120){
-        variableright--;
-      }
-  }
-  else if(lastright == '5' && timer[1]==0){
-      timer[1]++;
-      if(variableright > 60){
-        variableright--;
-      }
-      else if(variableright < 60){
-        variableright++;
-      }
-  }
-  else if(lastright == '7' && timer[1]==0){
-      timer[1]++;
-      if(variableright < 150){
-        variableright++;
-      }
-      else if(variableright > 150){
-        variableright--;
-      }  
-  }
-  else if(lastright == '9' && timer[1]==0){
-      timer[1]++;
-      if(variableright > 30){
-        variableright--;
-      }
-      else if(variableright < 30){
-        variableright++;
-      }
+  else if(currVal[1] < lastVal[1] ){
+    lastVal[1]--;
+    rightmotor.write(lastVal[1] );
   }
   else{
-    if(timer[1]>=COUNTER){
-      timer[1]=0;
-    }
-    else{
-      timer[1]++;
-    }
+    rightmotor.write(lastVal[1] );
   }
   
   //after serial data was read update all appropriate motors/servos/relays  
-  leftmotor.write(variableleft);
-  rightmotor.write(variableright);
-  armbase.write(armbasepos);
-  
-//  if(timer >=COUNTER){
-//    timer=0;
-//  }
-//  else{
-//    timer++;
-//  }
-
-  //Need to implement change based on time conditions are meet
-  //Issue: by the time code is updated, change to previous values have been made
-  //fix: need to intrduce currVal and lastVal that will keep track of changes(on my own time)
-  //fix2: add counter to section of code where motor speed is changed
-  //!!!!fix3: add counter to left and right wheel, only updates after every nth loop!!!!
-
-  //This count is just a very unprofessional way to make the robot stop if it hasn't recieved input from the controller after so many clock cycles [safety check]
-//  countl++;
-//  countr++;
-//  if(countl >= 5000){
-//    lastleft = '0';
-//  }
-//  if(countr >= 5000){
-//    lastright = '1';
-//  }
+  // armbase.write(armbasepos);
 }
